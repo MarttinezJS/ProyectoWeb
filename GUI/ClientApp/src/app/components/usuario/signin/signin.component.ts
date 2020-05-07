7d7b6861aefb442168ae99906102211a701bca6c
Duvan <duvandaxx@gmail.com>
2020-05-06 23:29:36 -0500

    validaciones y sweetalert2

--- GUI/ClientApp/src/app/components/usuario/signin/signin.component.ts
+++ GUI/ClientApp/src/app/components/usuario/signin/signin.component.ts
@@ -3,6 +3,8 @@ import { FormGroup, FormBuilder, Validators, FormControlName} from '@angular/for
 import { Usuario } from '../../models/Usuario';
 import { UsuarioService } from '../../../services/usuario.service';
 
+import Swal from 'sweetalert2';
+
 @Component({
   selector: 'app-signin',
   templateUrl: './signin.component.html',
@@ -19,24 +21,58 @@ export class SigninComponent {
    }
 
   onSubmit() {
+    Swal.fire({
+      allowOutsideClick: false,
+      icon: 'info',
+      text:'Espere por favor...'
+    });
+    Swal.showLoading();
+
     this.usuario = this.formGroup.value;
     this.usuario.id = new Date().getTime().toString();
     this._usuarioService.post(this.usuario).subscribe(p => {
       if (p != null) {
         p = this.usuario;
         alert(p.nombre + ' ha sido creado');
+        Swal.fire({
+          icon:'error',
+          title: 'Error al registrar',
+          text: 'Este correo ya esta registrado creado'
+        });
+      }else{
+        Swal.fire({
+          title: 'Registrado correctamente',
+          icon:'success' 
+        });
       }
     });
   }
 
+  get nombreNoValido(){
+    return this.formGroup.get('nombre').invalid && this.formGroup.get('nombre').touched
+  }
+  get passNoValido(){
+    return this.formGroup.get('pass').invalid && this.formGroup.get('pass').touched
+  }
+  get emailNoValido(){
+    return this.formGroup.get('email').invalid && this.formGroup.get('email').touched
+  }
+  get telefonoNoValido(){
+    return this.formGroup.get('telefono').invalid && this.formGroup.get('telefono').touched
+  }
+  get direccionNoValido(){
+    return this.formGroup.get('direccion').invalid && this.formGroup.get('direccion').touched
+  }
+
+
   private crearFormulario () {
 
     this.formGroup = this.fb.group({
-      nombre   : ['', Validators.required],
+      nombre   : ['', Validators.required, Validators.minLength(2)],
       correo   : ['', [Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')]],
-      pass     : ['', Validators.required],
-      telefono : [, Validators.required],
-      direccion: ['', Validators.required]
+      pass     : ['', Validators.required, Validators.minLength(6)],
+      telefono : [, Validators.required, Validators.minLength(10), Validators.pattern('[0-9]')],
+      direccion: ['', Validators.required, Validators.minLength(5)]
     });
   }
 }
