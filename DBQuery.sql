6f5f16bd5970fbc15e06b58ae46ca1b9da6e78a7
MartinezJS <jstevensmartinez@unicesar.edu.co>
2020-05-09 17:24:30 -0500

    .sql modificado

--- DBQuery.sql
+++ DBQuery.sql
@@ -13,7 +13,16 @@ create table productos(
 	precio int not null,
 	presentacion varchar(15) not null,
 	descripcion varchar(50) default (''),
-	id_provedor varchar(2),
+	id_provedor varchar(2) not null,
 	id_servicio varchar(2)
 )
 
+create table provedores(
+	id VARCHAR(2) not null primary key,
+	nombre varchar(30) not null
+)
+
+create table servicios(
+	id VARCHAR(2),
+	nombre varchar(10) not null
+)
